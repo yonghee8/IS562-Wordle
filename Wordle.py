dd65d8436ae7681337d0ca7e1cd69519ca536463
yonghee8 <lyh22@byu.edu>
2023-09-18 14:40:31 -0600

    Keyboard colors are not working

--- Wordle.py
+++ Wordle.py
@@ -6,7 +6,6 @@ BE SURE TO UPDATE THIS COMMENT WHEN YOU WRITE THE CODE.
 """
 
 import random
-
 from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS, CORRECT_COLOR, PRESENT_COLOR, MISSING_COLOR
 
@@ -18,6 +17,7 @@ def wordle():
 
     def enter_action(s):
         user_word = s.lower()  # Convert the entered word to lowercase for case-insensitive comparison
+        
         if user_word in FIVE_LETTER_WORDS:
             # Check if the user guessed the entire word correctly
             if user_word == random_word:
@@ -40,10 +40,16 @@ def wordle():
                 # Set colors for correct letters
                 for col in correct_letters:
                     gw.set_square_color(gw.get_current_row(), col, CORRECT_COLOR)
+                    gw.set_key_color(letter.upper(), CORRECT_COLOR)
 
                 # Set colors for letters that are present but in the wrong place
                 for col in present_letters:
                     gw.set_square_color(gw.get_current_row(), col, PRESENT_COLOR)
+                    gw.set_key_color(letter.upper(), PRESENT_COLOR)
+
+                for col in missing_letters:
+                    gw.set_square_color(gw.get_current_row(), col, MISSING_COLOR)
+                    gw.set_key_color(letter.upper(), MISSING_COLOR)
 
                 # Move on to the next row
                 gw.set_current_row(gw.get_current_row() + 1)
@@ -56,7 +62,7 @@ def wordle():
     gw.add_enter_listener(enter_action)
 
     # Call the function to display the random word
-    display_random_word(gw, random_word)
+    #display_random_word(gw, random_word)
 
 def display_random_word(gw, word):
     for col in range(N_COLS):
